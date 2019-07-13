7db233a504bb0a2bbf8cadc00da56cd9284e842c
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-07-13 10:42:55 -0700

    old strategy

--- nba.js
+++ nba.js
@@ -1,36 +1,21 @@
-//import {select, csv, ScaleLinear} from 'd3';
-
-//const svg = select('svg');
-
-//const width = +svg.attr('width');
-//const height = +svg.attr('height');
 
 
-//const render = data => {
-  //  svg.selectAll('rect').data(data)
-    //    .enter().append('rect')
-      //      .attr('width', 300)
-        //    .attr('height',300)
-//};
-
-
-//this code is place holder for inserting data locally. Mainly used for personal learning
-//CloseEvent('placeHolderData.csv').then(data => {
-  //  data.forEach(d => {
-    //    d.population = +d.population;
-    //});
-    //render(data);
-    //console.log(data)
-//})
-
-var myData = [100, 125, 244, 355, 500];
+var myData = [100, 125, 244, 355, 500, 20, 720, 643];
 
 var height = 500;
 var width = 500;
-var barWidth = 35;
-var barOffset = 5;
+//var barWidth = 35;
+//var barOffset = 5;
 
-var myChart = d3.select('#tableValue').append('svg')
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(myData)])
+    .range([0, height]);
+
+var xScale = d3.scaleOrdinal()
+    .domain([0, d3.range(0, myData.length)])
+    .range([0, width])
+
+d3.select('#tableValue').append('svg')
     .attr('width', width)
     .attr('height', height)
     .style('background', '#f4f4f4')
@@ -38,14 +23,14 @@ var myChart = d3.select('#tableValue').append('svg')
         .data(myData)
         .enter().append('rect')
             .style('fill', 'lightgreen')
-            .attr('width', barWidth)
+            .attr('width', xScale.range())
             .attr('height', function(d){
-                return d;
+                return yScale(d);
             })
             .attr('x', function(d, i){
-                return i * (barWidth + barOffset
-                    );
+                return x(i);
+                
             })
             .attr('y', function(d){
-                return height - d;
+                return height - yScale(d);
             })
