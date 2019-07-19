6826a7f6edd98bd608faefa6a6ba932ee45439be
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-07-18 23:09:31 -0700

    Graph Data Produced for one player

--- nba.js
+++ nba.js
@@ -1,155 +1,62 @@
-//Darioush's work
-
 window.onload = function () {
+var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    dataPointWidthMax: 20,
+    dataPointWidthMin: 5,
+    labelWrap: true,
 
-    var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        dataPointWidthMax: 20,
-        dataPointWidthMin: 5,
+    title: {
+        text: "",
+        fontFamily: "arial black",
+        fontColor: "#695A42"
+    },
+    axisX: {
         labelWrap: true,
+        labelAutoFit: true,
+    },
+    axisY: {
+    },
+    toolTip: {
+        shared: true,
+        content: "<br/>{name}: <strong>{y}</strong>mn Units"
+    },
+    
+    data: [{
+        type: "stackedColumn",
+        showInLegend: true,
+        color: "#696661",
+        name: '#newName',
+        dataPoints: [
+            //{"name":"LeBron James","team_acronym":"cle","team_name":"Cleveland Cavaliers","games_played":"46","minutes_per_game":"36:54","field_goals_attempted_per_game":"18.7","field_goals_made_per_game":"10.3","field_goal_percentage":"54.9","free_throw_percentage":"74.5","three_point_attempted_per_game":"4.9","three_point_made_per_game":"1.8","three_point_percentage":"36.2","points_per_game":"26.8","offensive_rebounds_per_game":"1.2","defensive_rebounds_per_game":"6.7","rebounds_per_game":"7.9","assists_per_game":"8.6","steals_per_game":"1.6","blocks_per_game":"1.1","turnovers_per_game":"4.3","player_efficiency_rating":"1.7"}
+            { y: 0, label: 'GP' },
+            { y: 0, label: 'MIN' },
+            { y: 0, label: 'FGM' },
+            { y: 0, label: 'FGA' },
+            { y: 0, label: 'FGPCT' },
+            { y: 0, label: 'FG3M' },
+            { y: 0, label: 'FTM' },
+            { y: 0, label: 'FTA' },
+            { y: 0, label: 'FTPCT' },
+            { y: 0, label: 'OREB' },
+            { y: 0, label: 'DREB' },
+            { y: 0, label: 'REB' },
+            { y: 0, label: 'AST' },
+            { y: 0, label: 'STL' },
+            { y: 0, label: 'BLK' },
+            { y: 0, label: 'TO' },
+            { y: 0, label: 'PF' },
+            { y: 0, label: 'PT' },
 
-        title: {
-            text: "",
-            fontFamily: "arial black",
-            fontColor: "#695A42"
-        },
-        axisX: {
-            labelWrap: true,
-            labelAutoFit: true,
-        },
-        axisY: {
-        },
-        toolTip: {
-            shared: true,
-            content: "<br/>{name}: <strong>{y}</strong>mn Units"
-        },
-        data: [{
-            type: "stackedColumn",
-            showInLegend: true,
-            color: "#696661",
-            name: "Lebron James",
-            dataPoints: [
-                { y: 45, label: 'GP' },
-                { y: 3, label: 'MIN' },
-                { y: 22, label: 'FGM' },
-                { y: 17, label: 'FGA' },
-                { y: 4, label: 'FGPCT' },
-                { y: 6, label: 'FG3M' },
-                { y: 9, label: 'FTM' },
-                { y: 2, label: 'FTA' },
-                { y: 4, label: 'FTPCT' },
-                { y: 10, label: 'OREB' },
-                { y: 20, label: 'DREB' },
-                { y: 3, label: 'REB' },
-                { y: 5, label: 'AST' },
-                { y: 19, label: 'STL' },
-                { y: 4, label: 'BLK' },
-                { y: 6, label: 'TO' },
-                { y: 8, label: 'PF' },
-                { y: 12, label: 'PT' },
-
-            ]
-        },
-        {
-            type: "stackedColumn",
-            showInLegend: true,
-            name: "Dwayne Wade",
-            color: "#EDCA93",
-            dataPoints: [
-                { y: 45, label: 'GP' },
-                { y: 3, label: 'MIN' },
-                { y: 22, label: 'FGM' },
-                { y: 17, label: 'FGA' },
-                { y: 4, label: 'FGPCT' },
-                { y: 6, label: 'FG3M' },
-                { y: 9, label: 'FTM' },
-                { y: 2, label: 'FTA' },
-                { y: 4, label: 'FTPCT' },
-                { y: 10, label: 'OREB' },
-                { y: 20, label: 'DREB' },
-                { y: 3, label: 'REB' },
-                { y: 5, label: 'AST' },
-                { y: 19, label: 'STL' },
-                { y: 4, label: 'BLK' },
-                { y: 6, label: 'TO' },
-                { y: 8, label: 'PF' },
-                { y: 12, label: 'PT' },
-            ]
-        },
-        {
-            type: "stackedColumn",
-            showInLegend: true,
-            name: "Kawhi Leonard",
-            color: "#695A42",
-            dataPoints: [
-                { y: 45, label: 'GP' },
-                { y: 3, label: 'MIN' },
-                { y: 22, label: 'FGM' },
-                { y: 17, label: 'FGA' },
-                { y: 4, label: 'FGPCT' },
-                { y: 6, label: 'FG3M' },
-                { y: 9, label: 'FTM' },
-                { y: 2, label: 'FTA' },
-                { y: 4, label: 'FTPCT' },
-                { y: 10, label: 'OREB' },
-                { y: 20, label: 'DREB' },
-                { y: 3, label: 'REB' },
-                { y: 5, label: 'AST' },
-                { y: 19, label: 'STL' },
-                { y: 4, label: 'BLK' },
-                { y: 6, label: 'TO' },
-                { y: 8, label: 'PF' },
-                { y: 12, label: 'PT' },
-            ]
-        },
-        {
-            type: "stackedColumn",
-            showInLegend: true,
-            name: "Steph Curry",
-            color: "#B6B1A8",
-            dataPoints: [
-                { y: 45, label: 'GP' },
-                { y: 3, label: 'MIN' },
-                { y: 22, label: 'FGM' },
-                { y: 17, label: 'FGA' },
-                { y: 4, label: 'FGPCT' },
-                { y: 6, label: 'FG3M' },
-                { y: 9, label: 'FTM' },
-                { y: 2, label: 'FTA' },
-                { y: 4, label: 'FTPCT' },
-                { y: 10, label: 'OREB' },
-                { y: 20, label: 'DREB' },
-                { y: 3, label: 'REB' },
-                { y: 5, label: 'AST' },
-                { y: 19, label: 'STL' },
-                { y: 4, label: 'BLK' },
-                { y: 6, label: 'TO' },
-                { y: 8, label: 'PF' },
-                { y: 12, label: 'PT' },
-            ]
-        }]
-    });
-    chart.render();
-
-    //orginal function for data mouse over window
-    // function toolTipContent(e) {
-    //     var str = "";
-    //     var total = 0;
-    //     var str2, str3;
-    //     for (var i = 0; i < e.entries.length; i++){
-    //         var  str1 = "<span style= \"color:"+e.entries[i].dataSeries.color + "\"> "+e.entries[i].dataSeries.name+"</span>: $<strong>"+e.entries[i].dataPoint.y+"</strong>bn<br/>";
-    //         total = e.entries[i].dataPoint.y + total;
-    //         str = str.concat(str1);
-    //     }
-    //     str2 = "<span style = \"color:DodgerBlue;\"><strong>"+(e.entries[0].dataPoint.x).label()+"</strong></span><br/>";
-    //     total = Math.round(total * 100) / 100;
-    //     str3 = "<span style = \"color:Tomato\">Total:</span><strong> $"+total+"</strong>bn<br/>";
-    //     return (str2.concat(str)).concat(str3);
-    // }
-
-}
+        ]
+    },
+    
+    
+    ]
+});
+chart.render();
 
 //Evans work
+
 $("#find-player").on("click", function (event) {
     $.ajax({
         url: "https://nba-players.herokuapp.com/players-stats",
@@ -190,10 +97,66 @@ $("#find-player").on("click", function (event) {
             return false;
         }
         else {
+            console.log(foundPlayerData.games_played);
+            chart = new CanvasJS.Chart("chartContainer", {
+                animationEnabled: true,
+                dataPointWidthMax: 20,
+                dataPointWidthMin: 5,
+                labelWrap: true,
+        
+                title: {
+                    text: "",
+                    fontFamily: "arial black",
+                    fontColor: "#695A42"
+                },
+                axisX: {
+                    labelWrap: true,
+                    labelAutoFit: true,
+                },
+                axisY: {
+                },
+                toolTip: {
+                    shared: true,
+                    content: "<br/>{name}: <strong>{y}</strong>"
+                },
+                data: [{
+                    type: "stackedColumn",
+                    showInLegend: true,
+                    color: "#696661",
+                    name: '#newName',
+                    dataPoints: [
+                        //{"name":"LeBron James","team_acronym":"cle","team_name":"Cleveland Cavaliers",1"games_played":"46",2"minutes_per_game":"36:54",3"field_goals_attempted_per_game":"18.7",4"field_goals_made_per_game":"10.3",5"field_goal_percentage":"54.9",6"free_throw_percentage":"74.5",7"three_point_attempted_per_game":"4.9",8"three_point_made_per_game":"1.8","three_point_percentage":"36.2","points_per_game":"26.8","offensive_rebounds_per_game":"1.2","defensive_rebounds_per_game":"6.7","rebounds_per_game":"7.9","assists_per_game":"8.6","steals_per_game":"1.6","blocks_per_game":"1.1","turnovers_per_game":"4.3","player_efficiency_rating":"1.7"}
+
+                        //console.log(foundPlayerData.assists_per_game, "assist");
+
+                        { y: parseInt(foundPlayerData.games_played), label: 'GP' },
+                        { y: parseInt(foundPlayerData.minutes_per_game), label: 'MIN' },
+                        { y: parseInt(foundPlayerData.field_goals_made_per_game), label: 'FGM' },
+                        { y: parseInt(foundPlayerData.field_goals_attempted_per_game), label: 'FGA' },
+                        { y: parseInt(foundPlayerData.field_goal_percentage), label: 'FGPCT' },
+                        { y: parseInt(foundPlayerData.three_point_made_per_game), label: 'FG3M' },
+                        { y: parseInt(foundPlayerData.free_throw_percentage), label: 'FT%' },
+                        { y: parseInt(foundPlayerData.points_per_game), label: 'PPG' },
+                        { y: parseInt(foundPlayerData.offensive_rebounds_per_game), label: 'DREB' },
+                        { y: parseInt(foundPlayerData.defensive_rebounds_per_game), label: 'OREB' },
+                        { y: parseInt(foundPlayerData.rebounds_per_game), label: 'REB' },
+                        { y: parseInt(foundPlayerData.assists_per_game), label: 'AST' },
+                        { y: parseInt(foundPlayerData.steals_per_game), label: 'STL' },
+                        { y: parseInt(foundPlayerData.blocks_per_game), label: 'BLK' },
+                        { y: parseInt(foundPlayerData.turnovers_per_game), label: 'TO' },
+                        { y: parseInt(foundPlayerData.player_efficiency_rating), label: 'PT' },
+        
+                    ]
+                },
+                
+                
+                ]
+            });
+            chart.render();
+
             playerView.append(`<div class=${newName}>${JSON.stringify(foundPlayerData)}</div>`);
             var playerName = name;
 
-            console.log(name);
 
             var splitPlayerName = playerName.split(' ');
 
@@ -221,9 +184,32 @@ $("#find-player").on("click", function (event) {
             $("#buttonArea").append(addButton);
 
 
-            addButton.on('click', function () { $('.' + newName).remove(); });
-        }
+            addButton.on('click', function () { $('.' + newName).remove()
 
+            console.log("hello")
+            ; });
+        }
 
     });
 });
+
+//Darioush's work
+
+
+    //orginal function for data mouse over window
+    // function toolTipContent(e) {
+    //     var str = "";
+    //     var total = 0;
+    //     var str2, str3;
+    //     for (var i = 0; i < e.entries.length; i++){
+    //         var  str1 = "<span style= \"color:"+e.entries[i].dataSeries.color + "\"> "+e.entries[i].dataSeries.name+"</span>: $<strong>"+e.entries[i].dataPoint.y+"</strong>bn<br/>";
+    //         total = e.entries[i].dataPoint.y + total;
+    //         str = str.concat(str1);
+    //     }
+    //     str2 = "<span style = \"color:DodgerBlue;\"><strong>"+(e.entries[0].dataPoint.x).label()+"</strong></span><br/>";
+    //     total = Math.round(total * 100) / 100;
+    //     str3 = "<span style = \"color:Tomato\">Total:</span><strong> $"+total+"</strong>bn<br/>";
+    //     return (str2.concat(str)).concat(str3);
+    // }
+
+}
